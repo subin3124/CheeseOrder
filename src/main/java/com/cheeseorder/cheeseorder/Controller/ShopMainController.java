a945e359bc27152a47af8c2aa375847c6af21ce1
subin <subibi21@inu.ac.kr>
2023-04-12 17:37:48 +0900

    controller to rest controller

--- src/main/java/com/cheeseorder/cheeseorder/Controller/ShopMainController.java
+++ src/main/java/com/cheeseorder/cheeseorder/Controller/ShopMainController.java
@@ -3,16 +3,10 @@ package com.cheeseorder.cheeseorder.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ShopMainController {
 
 
-
-    @GetMapping("/Shop/{ShopId}")
-    private String MainShopView(@PathVariable(name = "ShopId") String ShopId) {
-
-
-        return "MainShopView";
-    }
 }
